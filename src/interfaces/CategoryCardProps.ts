4266a429025842ad52a4aa448af4a9428a44fbc8
richardtekula <barspin4499@gmail.com>
2025-09-27 08:14:44 +0200

    change one types instead interfaces & added category images

--- src/interfaces/CategoryCardProps.ts
+++
@@ -1,9 +0,0 @@
-export interface CategoryCardProps {
-  category: {
-    id: string;
-    name: string;
-    englishName: string;
-    description: string;
-    color: string;
-  }
-}
--- src/interfaces/FloatingParticlesProps.ts
+++
@@ -1,3 +0,0 @@
-export interface FloatingParticleProps {
-  delay: number;
-}
